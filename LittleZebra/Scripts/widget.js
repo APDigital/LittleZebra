efb91cc9a36fc786300d6189700accf98b95b55e
Aoife Pearson <aoifejo.pearson@googlemail.com>
2018-01-08 21:46:02 +0000

    Added Javascript Functionality

---
+++ LittleZebra/Scripts/widget.js
@@ -0,0 +1,30 @@
+﻿$(function disable() {
+    $('#searchInput').keyup(function () {
+        if ($(this).val() == '') {
+            $('.disableOnInput').prop('disabled', false);
+        }
+        else {
+            $('.disableOnInput').prop('disabled', true);
+        }
+    })
+});
+$(function hidden() {
+    $('#searchInput').keyup(function () {
+        if ($(this).val() <= 10) {
+            $('.warn').prop('hidden', false);
+        }
+        if ($(this).val() == '') {
+            $('.warn').prop('hidden', true);
+        }
+        if ($(this).val() >= 11) {
+            $('.warn').prop('hidden', true);
+        }
+    })
+});
+$(document).ready(function () {
+    $("#animalSelect").change(function () {
+        $("img[name=image-swap]").attr("src", $(this).val());
+
+    });
+
+});
